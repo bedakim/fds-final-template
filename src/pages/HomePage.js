f3c5c6eab1465102a4a1385c2fe3bc47e3f756ca
Kim Minyeob <stankim0525@gmail.com>
2018-11-30 11:22:06 +0900

    logout complete use for key change

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ProductList from '../containers/ProductList';
 import Layout from '../components/Layout';
 
+// 강제로 상태를 초기화 하고 싶을때 다른키 를 넣어줘야함 username
 export default class HomePage extends Component {
   render() {
     return (
